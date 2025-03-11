cf14b50afe7e614ef4c0823a8d496de4b6827907
DewaPuspito <dewactra02@gmail.com>
2025-03-11 13:19:37 +0700

    fixing filters

--- src/components/molecules/product.module.tsx
+++ src/components/molecules/product.module.tsx
@@ -6,24 +6,20 @@ import ProductCard, { PCard } from "../atomics/product.card.module";
 export default function ProductMenu() {
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedGenre, setSelectedGenre] = useState("All Genres");
+  const [selectedConsole, setSelectedConsole] = useState("All Consoles");
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const [filteredProducts, setFilteredProduct] = useState<PCard[]>([]);
-  const [genres, setGenres] = useState<string[]>([]);
+  const genres = ["Action", "Adventure", "RPG", "Fighting", "Horror", "Survival", "Stealth"];
+  const consoles = ["PC", "PS", "PSP", "Nintendo", "XBOX", "Wii"];
 
   const getAllProducts = useCallback(async () => {
     setIsLoading(true);
     try {
       const response = await axiosInstance.get("/products?pageSize=20");
-      const uniqueGenres: string[] = [];
-      response.data.forEach((item: PCard) => {
-        if (!uniqueGenres.includes(item.genre)) {
-          uniqueGenres.push(item.genre);
-        }
-      });
-      setGenres(uniqueGenres);
 
       let filtered = response.data;
+
       if (searchTerm) {
         filtered = filtered.filter((item: PCard) =>
           item.game_name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -32,17 +28,24 @@ export default function ProductMenu() {
 
       if (selectedGenre !== "All Genres") {
         filtered = filtered.filter(
-          (item: PCard) => item.genre === selectedGenre
+          (item: PCard) =>  item.genre.toLowerCase().includes(selectedGenre.toLowerCase())
         );
       }
-      setFilteredProduct(filtered);
-    } catch (error) {
-      console.error("Error fetching products:", error);
-      alert("Maaf data tidak bisa diambil. Silakan cek sumber data");
-    } finally {
-      setIsLoading(false);
-    }
-  }, [searchTerm, selectedGenre]);
+
+      if (selectedConsole!== "All Consoles") {
+        filtered = filtered.filter(
+          (item: PCard) =>  item.console.toLowerCase().includes(selectedConsole.toLowerCase())
+        );
+      }
+
+        setFilteredProduct(filtered);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        alert("Maaf data tidak bisa diambil. Silakan cek sumber data");
+      } finally {
+        setIsLoading(false);
+      }
+    }, [searchTerm, selectedGenre, selectedConsole]);
 
   useEffect(() => {
     getAllProducts();
@@ -56,13 +59,17 @@ export default function ProductMenu() {
     setSelectedGenre(event.target.value);
   }
 
+  function handleConsoleChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    setSelectedConsole(event.target.value);
+  }
+
   return (
     <div className="bg-container w-full min-h-screen p-3">
       <h2 className="flex justify-center text-3xl text-white items-center font-bold mt-5 mb-10">
         {" "}
         Produk Kami{" "}
       </h2>
-      <div className="flex justify-between mb-4">
+      <div className="flex items-center justify-between mb-4 gap-4">
         <input
           type="text"
           placeholder="Search products..."
@@ -70,8 +77,10 @@ export default function ProductMenu() {
           value={searchTerm}
           onChange={handleSearchChange}
         />
+
+        <div className="flex gap-4">
         <select
-          className="border p-2 rounded-md w-1/3 bg-white text-black"
+          className="border p-2 rounded-md w-42 bg-white text-black"
           value={selectedGenre}
           onChange={handleGenreChange}
         >
@@ -82,6 +91,20 @@ export default function ProductMenu() {
             </option>
           ))}
         </select>
+
+        <select
+          className="border p-2 rounded-md w-42 bg-white text-black"
+          value={selectedConsole}
+          onChange={handleConsoleChange}
+        >
+          <option value="All Consoles">All Consoles</option>
+          {consoles.map((item, key) => (
+            <option key={key} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+        </div>
       </div>
       {isLoading === false ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-center items-center w-full">
@@ -99,7 +122,7 @@ export default function ProductMenu() {
             ))
           ) : (
             <div className="col-span-6 flex justify-center items-center min-h-[50vh]">
-              <p className="text-center text-white">No products found</p>
+              <p className="text-center text-white">Produk tidak ditemukan</p>
             </div>
           )}
         </div>
